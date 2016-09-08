417a654f5d9e7102a7514d4b232ff4da41694017
Manali Kekre <manali.kekre@gmail.com>
2016-09-08 13:47:10 +0000

    basic setup done

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!\n'
+@app.route('/halloffame')
+def hall_of_fame():
+    '''
+    Returns top 5 best players ranked by score
+
+    Returns- dict
+    '''
+    result = {
+        "message":"Nothing to show here",
+        "error":"None"
+    }
+    return jsonify(result)
+
+def process(data):
+    result  = '''
+    "message":"submission successful",
+    "data": {
+            "name":data['name'],
+            "text":data['text']
+    },
+    "error":"None"''
+    '''
+    status = 201
+    return result, status
+
+@app.route('/play', methods=['POST'])
+def play():
+
+    # get data from request
+    data = json.loads(request.data)
+
+    print "Received- ", data
+
+    result, status = process(data)
+
+    return jsonify(data), status
