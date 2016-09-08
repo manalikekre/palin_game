25fa69ab243b6e4c04e74529d1c802bbe899b7ba
Manali Kekre <manali.kekre@gmail.com>
2016-09-08 15:01:20 +0000

    implements redis as shared storage

--- main.py
+++ main.py
@@ -1,30 +1,82 @@
 from flask import Flask, jsonify, request
 import json
 
+# setup redis
+import redis
+redis_hndlr = redis.Redis(host='localhost', port=6379, db=0)
+
+# setup flask
 app = Flask(__name__)
+
+def get_score(text):
+    '''
+    Computes the score corresponding to given text
+
+    TBD, implement is_palindrome logic here
+    :param text: string
+    :return: integer
+    '''
+    return len(text)
+
+def get_data(user='all'):
+    '''
+    Retrieves data from redis in-memory data
+
+    By default, fetches all the data, and if
+    name is given, fetches score corresponding
+    to that user
+
+    :param name: string
+    :return: dict
+    '''
+    print 'inside get_data'
+    print 'user- ', user
+    if user == 'all':
+        return redis_hndlr.hgetall('users')
+    else:
+        return redis_hndlr.hget('users', user)
+
+def put_data(data):
+    print 'inside put_data'
+    print 'data- ', data
+
+    redis_hndlr.hincrby('users', data['name'], get_score(data['text']))
+
+    current_count = get_data(user=data['name'])
+    print 'current_count- ', current_count
+
+    return {
+        data['name']:current_count
+    }
 
 @app.route('/halloffame')
 def hall_of_fame():
     '''
     Returns top 5 best players ranked by score
-
-    Returns- dict
     '''
-    result = {
-        "message":"Nothing to show here",
-        "error":"None"
-    }
-    return jsonify(result)
+    print 'inside hall_of_fame'
+    all_data = get_data(user='all')
+
+    top_five = sorted(all_data.items(), key=all_data.get, reverse=True)
+    return jsonify(top_five)
+
+@app.route('/all')
+def get_all():
+    '''
+    Returns complete user data
+    '''
+    print 'inside get_all'
+    return jsonify(get_data(user='all'))
 
 def process(data):
-    result  = '''
+
+    temp_result = put_data(data)
+
+    result  = {
     "message":"submission successful",
-    "data": {
-            "name":data['name'],
-            "text":data['text']
-    },
+    "data": temp_result,
     "error":"None"''
-    '''
+    }
     status = 201
     return result, status
 
@@ -38,4 +90,4 @@ def play():
 
     result, status = process(data)
 
-    return jsonify(data), status
+    return jsonify(result), status
