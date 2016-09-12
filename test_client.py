3d4b36a842ee2028d8c8a934b686518ca41c95c6
Manali Kekre <manali.kekre@gmail.com>
2016-09-12 12:36:31 +0000

    adds multithreaded test case for play

--- test_client.py
+++ test_client.py
@@ -2,14 +2,31 @@ from multiprocessing.dummy import Pool as ThreadPool
 import client
 import time
 import random
+import main
 
-NO_OF_WORKERS = 4
+def test_get_user():
+    '''Tests if a user's score persists & is retrievable'''
+    pass
 
-if __name__ == '__main__':
-    no_of_workers = NO_OF_WORKERS
-    # Make the Pool of workers
-    pool = ThreadPool(no_of_workers)
+def test_play():
+    '''Tests if a user's score is incremented properly'''
+    pass
 
+def test_halloffame():
+    '''Tests if halloffame correctly returns top5 users'''
+    pass
+
+def test_threadsafety_for_play():
+    '''Tests if score is consistent if multiple clients
+    hit the server at the same time'''
+
+    print '\nTesting Threadsafety'
+
+    # reset data on redis database for testing
+    main.reset_data()
+
+    # prepare sample data
+    # make sure all text are palindrome for test case to work
     sample_data =  [
         {
             "name": "abcd",
@@ -28,27 +45,49 @@ if __name__ == '__main__':
             "text": "abcddcba"
         },
     ]
-    data = []
-    counts = [50, 100, 200, 500]
 
+    user_names = [item['name'] for item in sample_data]
+    counts = [50, 100, 200, 500]
+    scores = [len(item["text"])/2.0 for item in sample_data]
+
+    expected_scores = [count*score for count, score in zip(counts, scores)]
+
+    data = []
     for i in range(4):
         for count in range(counts[i]):
             data.append(sample_data[i])
-
-    user_names = [item['name'] for item in data]
-    # randomize the data
     random.shuffle(data)
-    random.shuffle(user_names)
+
+    # Make the Pool of workers
+    no_of_workers = 4
+    pool = ThreadPool(no_of_workers)
 
     start_time = time.time()
     # Open the urls in their own workers
     # and return the results
-    #results = pool.map(client.play, data)
-    results = pool.map(client.get_user, user_names)
+    print '\nInitiating ', no_of_workers,' threads to hit the server ', sum(counts), ' times...'
+    results = pool.map(client.play, data)
+
     # close the pool and wait for the work to finish
     pool.close()
     pool.join()
     end_time = time.time()
 
-    print '\nworkers- ', no_of_workers
-    print '\ntook- ', (end_time - start_time), ' seconds\n'
+    print '\nTook- ', round((end_time - start_time),3), ' seconds\n'
+    #print 'user_names- ', user_names
+    #print 'counts- ', counts
+    #print 'scores- ', scores
+    #print 'expected_scores- ', expected_scores
+    received_scores = [float(client.get_user(user)['data']["score"]) for user in user_names]
+
+    #print 'received_scores- ', received_scores
+
+    if received_scores == expected_scores:
+        print 'TEST SUCCESSFUL- Data is consistent!\n'
+    else:
+        print 'TEST FAILED- Data is inconsistent!\n'
+
+if __name__ == '__main__':
+    test_threadsafety_for_play()
+
+
